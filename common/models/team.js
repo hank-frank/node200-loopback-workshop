fc7a2bfe7ecdbca165e164dc55dfbb3dae93b345
hankfrank <henryfrank1@yahoo.com>
2019-07-27 10:46:19 -0700

    All working files, tests passing.

---
+++ common/models/team.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function(Team) {
+
+    // inside the code block that is being exported
+    //this will disable a Remote Method. 
+// {Team}.disableRemoteMethod("delete", true);
+
+};
